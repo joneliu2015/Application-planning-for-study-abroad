4f5c168960707efb798bed0c7ae52a3e1e5b039d
joinliu <1028301097@qq.com>
2017-12-27 17:29:01 +0800

    添加了后面的的结构和js

---
+++ js/index.js
@@ -0,0 +1,72 @@
+//乐语弹窗
+function openWin() {
+    window.open('http://chat.looyuoms.com/chat/chat/p.do?c=20002134&f=10071643&g=10068659&site=15890&refer=biaoji&loc=biaoji', '', 'height=500, width=800,top=200, left=300,  toolbar =no, menubar=no, scrollbars=no, resizable=no, location=no, status=no')
+};
+//点击返回顶部
+$(".toTop").click(function() {
+    $("html,body").animate({
+        scrollTop: 0
+    }, 500);
+});
+//底部二维码显示效果
+$('.wechat').mouseenter(function() {
+    $('.qrwechat').show()
+});
+$('.weibo').mouseenter(function() {
+    $('.qrweibo').show()
+})
+$('.wechat').mouseleave(function() {
+    $('.qrwechat').hide()
+});
+$('.weibo').mouseleave(function() {
+    $('.qrweibo').hide()
+})
+
+window.onload=function(){
+    var data;
+    $.ajax({
+        dataType: 'json',
+		url: 'data.json',
+		data: data,
+        type: 'get',
+        success:function(data,key){
+            var jsArr=data;
+            for(var key in jsArr){};
+            console.log(jsArr);
+            $("body").on("click", ".country>li", function(e){
+                //获取当前tab中鼠标停在的table栏的索引  
+                var index = $(this).index();
+                console.log(index);
+                //根据这个索引切换，下面的展示  
+                $(".country>li").eq(index).addClass("active").siblings().removeClass("active");
+                //获取当前点击项目的ID
+                var indexI = $(this).index();
+                console.log(indexI);
+                console.log(jsArr);
+               
+                var obj={
+                    items:jsArr.country[indexI]
+                };
+                console.log(obj);
+
+                var result=template('template',obj);
+                $('section').html('');
+                $('nav').after(result);
+
+                $('.center').on('click','.center_title>span',function(e){
+                    var index2=$(this).index();
+                    console.log(index2);
+                    $(".center_title>span").eq(index2).addClass("active2").siblings().removeClass("active2");
+                    $(".center_title>span").eq(index2).addClass("change").siblings().removeClass("change");
+                    $(".center_content>img").eq(index2).show();
+                    $(".center_content>img").eq(index2).siblings().hide()
+                });
+                $(".grade").trigger('click')    
+
+
+            });
+            $('#Americia').trigger('click');
+            
+        }
+    })
+}
